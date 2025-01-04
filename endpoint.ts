f5681a1bfd50ba0f301d82cf0e9fe0491f75f3a8
0xk3rn3lp4nic <dev@itsrabayadg.com>
2025-01-04 23:18:03 +0800

    Refactor endpoint configuration to modularize middleware and routes, enhancing maintainability and readability

--- endpoint.ts
+++ endpoint.ts
@@ -1,46 +1,50 @@
-import os from "os";
+"use server";
+
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import path from "path";
-import https from "https";
-import fs from "fs";
-import rateLimit from "express-rate-limit";
+
 import helmet from "helmet";
-import morgan from "morgan";
 import hpp from "hpp";
 import mongoSanitize from "express-mongo-sanitize";
 import express, { Request, Response } from "express";
-import { ErrorRequestHandler } from "express";
 import passport from "passport";
 import session from "express-session";
 import { Server } from "socket.io";
 
 // Import middleware and routes
-import { userMiddleware } from "./api/v1/middleware/user/middleware";
-import userAuthRouter from "./api/v1/routes/user/auth/auth.route";
-import userProfileRouter from "./api/v1/routes/user/user/user.credential";
-import tokenValidRouter from "./api/v1/routes/user/auth/token.valid.route";
-import teamRouter from "./api/v1/routes/team/team.route";
 import { authRateLimiter, limiter } from "./limiter";
+import { sessionConfig } from "./src/config/session";
+import { helmetOptions } from "./src/config/helmet";
+import { corsOptions } from "./src/config/cors";
+import { logger } from "./src/config/logger";
+import { errorHandler } from "./src/middleware/errorHandler";
+import { setupTeamSocket } from "./src/sockets/team.socket";
+import { userMiddleware } from "./src/middleware/userMiddleware";
+import userAuthRouter from "./src/api/v1/routes/user/auth/auth.route";
+import tokenValidRouter from "./src/api/v1/routes/user/auth/token.valid.route";
+import userProfileRouter from "./src/api/v1/routes/user/user/user.credential";
+
+// Import team routes
+import createTeamRouter from "./src/api/v1/routes/team/create.route";
+import joinTeamRouter from "./src/api/v1/routes/team/join.route";
+import listsTeamRouter from "./src/api/v1/routes/team/lists.route";
+import myTeamRouter from "./src/api/v1/routes/team/my-team.route";
+import rejectTeamRouter from "./src/api/v1/routes/team/reject.route";
+import checkTeamRouter from "./src/api/v1/routes/team/check-team.route";
+import acceptTeamRouter from "./src/api/v1/routes/team/accept.route";
+import teamDetailsRouter from "./src/api/v1/routes/team/teamdetails.route";
+import { app, httpsServer } from "./src/config/server";
 
 // Load environment variables
 dotenv.config();
 // Initialize Express app
-const app = express();
+
 const PORT: number | string = process.env.PORT || 5000;
 
-// Create HTTPS server
-const httpsServer = https.createServer(
-  {
-    key: fs.readFileSync("./ssl/private.key"),
-    cert: fs.readFileSync("./ssl/certificate.crt"),
-  },
-  app
-);
-
 // Initialize Socket.IO with the HTTPS server
-const io = new Server(httpsServer, {
+export const io = new Server(httpsServer, {
   cors: {
     origin: process.env.ALLOWED_ORIGINS?.split(","), // Allow frontend origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -49,71 +53,24 @@ const io = new Server(httpsServer, {
 });
 
 app.use(passport.initialize());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-    },
-  })
-);
-
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "https:"],
-      },
-    },
-    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
-    frameguard: { action: "deny" },
-    noSniff: true,
-    ieNoOpen: true,
-    xssFilter: true,
-  })
-);
-
-// Create a writable stream for logging
-const logStream = fs.createWriteStream(
-  path.join(__dirname, "logs", "access.log"),
-  {
-    flags: "a",
-    encoding: "utf8",
-    mode: 0o666,
-  }
-);
-
-// Logger
-app.use(morgan("combined", { stream: logStream }));
-// Prevent parameter pollution
+app.use(session(sessionConfig));
+app.use(helmet(helmetOptions));
+app.use(logger);
 app.use(hpp());
-// Sanitize data
 app.use(mongoSanitize());
-// Middleware
-app.use(
-  cors({
-    origin: process.env.ALLOWED_ORIGINS?.split(","),
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    maxAge: 600,
-  })
-);
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(limiter);
-// Health check endpoint
+app.use(errorHandler);
+
+/**
+ * Server Endpoints
+ */
+
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({ status: "OK" });
 });
-
 app.use(
   "/uploads",
   userMiddleware,
@@ -122,26 +79,20 @@ app.use(
 app.use("/api/v1/auth", authRateLimiter, userAuthRouter);
 app.use("/api/v1/token", authRateLimiter, tokenValidRouter);
 app.use("/api/v1/user", limiter, userMiddleware, userProfileRouter);
-app.use("/api/v1/team", limiter, userMiddleware, teamRouter);
 
-// Socket Listeners
+app.use("/api/v1/team/create", limiter, userMiddleware, createTeamRouter);
+app.use("/api/v1/team/join", limiter, userMiddleware, joinTeamRouter);
+app.use("/api/v1/team/accept", limiter, userMiddleware, acceptTeamRouter);
+app.use("/api/v1/team/reject", limiter, userMiddleware, rejectTeamRouter);
+app.use("/api/v1/team/list", limiter, userMiddleware, listsTeamRouter);
+app.use("/api/v1/team/my-team", limiter, userMiddleware, myTeamRouter);
+app.use("/api/v1/team/check-team", limiter, userMiddleware, checkTeamRouter);
+app.use("/api/v1/team/details", limiter, userMiddleware, teamDetailsRouter);
 
-io.on("connection", (socket) => {
-  socket.on("allTeams", () => {
-    io.emit("allTeams");
-  });
-});
+setupTeamSocket(io);
 
-// Error handling middleware
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-};
-
-app.use(errorHandler);
 // Start the HTTPS server
 httpsServer.listen(PORT as number, () => {
-  console.log(
-    `[SERVER] Secure server is running on https://localhost:${PORT}.`
-  );
+  console.log(`󱐋 [SERVER] - Server is running on https://localhost:${PORT}.`);
+  console.log(`󱐋 [SERVER] - Environment: ${process.env.NODE_ENV}.`);
 });
